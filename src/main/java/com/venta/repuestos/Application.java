7dc21d00be96ed58d34405e391e45ba70222e6c7
Davs07 <botsymeisterspieler@gmail.com>
2025-10-14 10:19:48 -0500

    my first changes

--- src/main/java/com/venta/repuestos/Application.java
+++ src/main/java/com/venta/repuestos/Application.java
@@ -1,13 +1,31 @@
 package com.venta.repuestos;
 
+import com.venta.repuestos.entidades.Cliente;
+import com.venta.repuestos.servicios.ClienteService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	//@Bean
+	CommandLineRunner start(ClienteService clienteService){
+		return args -> {
+			Stream.of("Cristian","Carlos","William","Shyntia").forEach(nombre ->{
+				Cliente clien = new Cliente();
+				clien.setNombre(nombre);
+				clien.setEmail(nombre.toLowerCase()+"@latinmail.com");
+				clienteService.save(clien);
+			});
+		};
 	}
 
 }
